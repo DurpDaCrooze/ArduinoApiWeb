71055293f808b9321fa20d1c89ffc8515b2abd85
DurpDaCrooze <cassarluca@gmail.com>
2021-09-07 03:23:52 +0200

    Changed api IP

--- loop.js
+++ loop.js
@@ -3,7 +3,7 @@ const slider = document.getElementById('bar');
 var value;
 
 function func(){
-  fetch('http://192.168.0.198:5000')
+  fetch('http://92.251.31.156:5000')
   .then(response => response.json())
   .then(data => value = data.arduinodata.anaologvalue);
 
